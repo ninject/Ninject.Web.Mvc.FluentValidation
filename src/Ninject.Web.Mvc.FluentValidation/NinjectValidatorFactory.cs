c1785208d48e7da362c353a479189d8b47ff1d66
Remo Gloor <remo.gloor@bbv.ch>
2010-10-21 02:37:32 +0200

    Added build scripts

--- src/Ninject.Web.Mvc.FluentValidation/NinjectValidatorFactory.cs
+++ src/Ninject.Web.Mvc.FluentValidation/NinjectValidatorFactory.cs
@@ -1,20 +1,36 @@
-﻿using System;
-using FluentValidation;
-
-namespace Ninject.Web.Mvc.FluentValidation
+﻿namespace Ninject.Web.Mvc.FluentValidation
 {
-	public class NinjectValidatorFactory : ValidatorFactoryBase
-	{
-		public NinjectValidatorFactory(IKernel kernel)
-		{
-			Kernel = kernel;
-		}
+    using System;
+    using global::FluentValidation;
 
-		public IKernel Kernel { get; set; }
+    /// <summary>
+    /// Validation factory that uses ninject to create validators  
+    /// </summary>
+    public class NinjectValidatorFactory : ValidatorFactoryBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NinjectValidatorFactory"/> class.
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        public NinjectValidatorFactory(IKernel kernel)
+        {
+            this.Kernel = kernel;
+        }
 
-		public override IValidator CreateInstance(Type validatorType)
-		{
-			return Kernel.Get(validatorType) as IValidator;
+        /// <summary>
+        /// Gets or sets the kernel.
+        /// </summary>
+        /// <value>The kernel.</value>
+        public IKernel Kernel { get; set; }
+
+        /// <summary>
+        /// Creates an instance of a validator with the given type using ninject.
+        /// </summary>
+        /// <param name="validatorType">Type of the validator.</param>
+        /// <returns>The newly created validator</returns>
+        public override IValidator CreateInstance(Type validatorType)
+        {
+            return this.Kernel.Get(validatorType) as IValidator;
 		}
 	}
 }
