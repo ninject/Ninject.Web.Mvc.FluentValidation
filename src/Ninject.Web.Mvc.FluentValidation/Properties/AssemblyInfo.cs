9d00c94f152fccd3eb5cebbac939fc3629af362d
Remo Gloor <remo.gloor@bbv.ch>
2011-12-28 05:39:19 +0100

    Added assembly description for nuget

---
+++ src/Ninject.Web.Mvc.FluentValidation/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Fluent validation extension for ninject")]
+[assembly: AssemblyDescription("Fluent validation extension for Ninject.Web.Mvc")]
